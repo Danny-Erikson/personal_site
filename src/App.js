32c1cc9c7b0976cbf2be110195f053284f6dcbf0
Danny-Erikson <derikson413@gmail.com>
2025-10-06 12:40:28 -0700

    prettier code reformat

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import { useState } from 'react';
-import './App.css';
-import portrait from './assets/portrait.png';
-import pythonlogo from './assets/pythonlogo.png';
-import javalogo from './assets/javalogo.svg';
-import { FaGithub, FaLinkedin, FaReact, FaGitAlt, FaHtml5, FaLinux} from "react-icons/fa";
+import { useState } from "react";
+import "./App.css";
+import portrait from "./assets/portrait.png";
+import pythonlogo from "./assets/pythonlogo.png";
+import javalogo from "./assets/javalogo.svg";
+import {
+  FaGithub,
+  FaLinkedin,
+  FaReact,
+  FaGitAlt,
+  FaHtml5,
+  FaLinux,
+} from "react-icons/fa";
 import { IoMailOutline, IoDocumentTextOutline } from "react-icons/io5";
 import { GrMysql } from "react-icons/gr";
 import { SiCsswizardry, SiIndeed } from "react-icons/si";
-import SkillShowcase from './components/skillShowcase.js';
-import ProjectShowcase from './components/ProjectShowcase.js';
-import Galaxy from './Galaxy';
+import SkillShowcase from "./components/skillShowcase.js";
+import ProjectShowcase from "./components/ProjectShowcase.js";
+import Galaxy from "./Galaxy";
 
 /* TODO: Fix File spacing*/
 /* TODO: Fix File structure*/
 
 function App() {
-const [hoverText, setHoverText] = useState("Quick Links");
+  const [hoverText, setHoverText] = useState("Quick Links");
 
   return (
     <div className="site">
@@ -23,88 +30,108 @@ const [hoverText, setHoverText] = useState("Quick Links");
         <Galaxy />
       </div>
       <div className="app">
-        <div className='nameplate'>
-          <div className='namecard'>
+        <div className="nameplate">
+          <div className="namecard">
             <h1>Hi, I'm Danny Erikson</h1>
-            <p>I'm a Full Stack Dev<br />In Los Angeles</p>
-            
-          <div className='links'>
-            <a 
-            href="https://github.com/"
-            target="_blank"
-            rel="noreferrer"
-            onMouseEnter={() => setHoverText("GitHub Profile")}
-            onMouseLeave={() => setHoverText("Quick Links")}>
-            <FaGithub size={69} />
-            </a>
-            <a 
-            href="https://www.indeed.com/"
-            target="_blank"
-            rel="noreferrer"
-            onMouseEnter={() => setHoverText("Indeed Profile")}
-            onMouseLeave={() => setHoverText("Quick Links")}>
-            <SiIndeed size={69} />
-            </a>
-            <a
-            href="https://www.linkedin.com/"
-            target="_blank"
-            rel="noreferrer"
-            onMouseEnter={() => setHoverText("LinkedIn Profile")}
-            onMouseLeave={() => setHoverText("Quick Links")}>
-              <FaLinkedin size={69} />
-            </a>
-            <a href="https://en.wikipedia.org/wiki/Gmail"
-            target="_blank"
-            rel="noreferrer"
-            onMouseEnter={() => setHoverText("Email Me")}
-            onMouseLeave={() => setHoverText("Quick Links")}>
-              <IoMailOutline size={69} />
-            </a>
-            <a 
-            href="https://en.wikipedia.org/wiki/R%C3%A9sum%C3%A9"
-            target="_blank"
-            rel="noreferrer"
-            onMouseEnter={() => setHoverText("Resume Download")}
-            onMouseLeave={() => setHoverText("Quick Links")}>
-              <IoDocumentTextOutline size={69} />
-            </a>
-            <p>{hoverText}</p>
+            <p>
+              I'm a Full Stack Dev
+              <br />
+              In Los Angeles
+            </p>
+
+            <div className="links">
+              <a
+                href="https://github.com/"
+                target="_blank"
+                rel="noreferrer"
+                onMouseEnter={() => setHoverText("GitHub Profile")}
+                onMouseLeave={() => setHoverText("Quick Links")}
+              >
+                <FaGithub size={69} />
+              </a>
+              <a
+                href="https://www.indeed.com/"
+                target="_blank"
+                rel="noreferrer"
+                onMouseEnter={() => setHoverText("Indeed Profile")}
+                onMouseLeave={() => setHoverText("Quick Links")}
+              >
+                <SiIndeed size={69} />
+              </a>
+              <a
+                href="https://www.linkedin.com/"
+                target="_blank"
+                rel="noreferrer"
+                onMouseEnter={() => setHoverText("LinkedIn Profile")}
+                onMouseLeave={() => setHoverText("Quick Links")}
+              >
+                <FaLinkedin size={69} />
+              </a>
+              <a
+                href="https://en.wikipedia.org/wiki/Gmail"
+                target="_blank"
+                rel="noreferrer"
+                onMouseEnter={() => setHoverText("Email Me")}
+                onMouseLeave={() => setHoverText("Quick Links")}
+              >
+                <IoMailOutline size={69} />
+              </a>
+              <a
+                href="https://en.wikipedia.org/wiki/R%C3%A9sum%C3%A9"
+                target="_blank"
+                rel="noreferrer"
+                onMouseEnter={() => setHoverText("Resume Download")}
+                onMouseLeave={() => setHoverText("Quick Links")}
+              >
+                <IoDocumentTextOutline size={69} />
+              </a>
+              <p>{hoverText}</p>
+            </div>
+          </div>
+          <div className="box-b">
+            <img src={portrait} alt="Danny Erikson" width="550" />
           </div>
         </div>
-        <div className='box-b'>
-          <img src={portrait} alt="Danny Erikson" width="550" />
-        </div>
-        </div>
-        <div className='skillcontainer'>
-        <h1 className='skilltitle' style={{ backgroundColor: 'dodgerblue'}} >Stuff I Work With</h1>
-        <div className='skillshowcase'>
-          <SkillShowcase 
-            icon={<FaReact size={65} color="#61DBFB"/>}
-            title={"Web Development"}
-            tech={"React"}
-            description={"React makes up the frontend of most of my projects. I love the component based architecture and the speed it provides."} 
-          />
-            <SkillShowcase 
-            icon={<img src={pythonlogo} alt="Python Logo" width={65} />}
-            title={"Backend Development"}
-            tech={"Python"}
-            description={"Python is my go to for backend development. I love the simplicity and readability of the language."}
-          />
-          <SkillShowcase 
-            icon={<GrMysql size={65} color="white"/>}
-            title={"Database Management"}
-            tech={"MySQL"}
-            description={"I primarily use MySQL for database management. I have experience with relational databases and perfer them to non-relational models."}
-          />
-        </div>
-        <h2 className='skilltitle' style={{ backgroundColor: '#6495ED'}}>Other stuff I work with</h2>
-        <div className='othertech'>
-          <img src={javalogo} alt="Java Logo" width={40} /> <p>Java</p>
-          <FaHtml5 size={40} color='#f06529'/> <p>HTML</p>
-          <SiCsswizardry size={40} color='#663399'/> <p>CSS</p>
-          <FaGitAlt size={40} color='#F1502F'/> <p>Git</p>
-          <FaLinux size={40}/> <p>Linux</p>
-        </div>
+        <div className="skillcontainer">
+          <h1 className="skilltitle" style={{ backgroundColor: "dodgerblue" }}>
+            Stuff I Work With
+          </h1>
+          <div className="skillshowcase">
+            <SkillShowcase
+              icon={<FaReact size={65} color="#61DBFB" />}
+              title={"Web Development"}
+              tech={"React"}
+              description={
+                "React makes up the frontend of most of my projects. I love the component based architecture and the speed it provides."
+              }
+            />
+            <SkillShowcase
+              icon={<img src={pythonlogo} alt="Python Logo" width={65} />}
+              title={"Backend Development"}
+              tech={"Python"}
+              description={
+                "Python is my go to for backend development. I love the simplicity and readability of the language."
+              }
+            />
+            <SkillShowcase
+              icon={<GrMysql size={65} color="white" />}
+              title={"Database Management"}
+              tech={"MySQL"}
+              description={
+                "I primarily use MySQL for database management. I have experience with relational databases and perfer them to non-relational models."
+              }
+            />
+          </div>
+          <h2 className="skilltitle" style={{ backgroundColor: "#6495ED" }}>
+            Other stuff I work with
+          </h2>
+          <div className="othertech">
+            <img src={javalogo} alt="Java Logo" width={40} /> <p>Java</p>
+            <FaHtml5 size={40} color="#f06529" /> <p>HTML</p>
+            <SiCsswizardry size={40} color="#663399" /> <p>CSS</p>
+            <FaGitAlt size={40} color="#F1502F" /> <p>Git</p>
+            <FaLinux size={40} /> <p>Linux</p>
+          </div>
         </div>
         <div>
           <h1>Work Experience</h1>
